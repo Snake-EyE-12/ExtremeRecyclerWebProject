c2a546617ab7b94c6679f4f583b4c560abfd3f74
Jacob Leonardo <leonardojacob257@gmail.com>
2024-02-09 17:00:13 -0700

    Bin & Item

---
+++ Models/Item.cs
@@ -0,0 +1,32 @@
+﻿namespace ExtremeRecycler.Models
+{
+	public class Item
+	{
+		public int id;
+		public string name = "Unnamed";
+		public bool recyclable = false;
+		public int capacity = 1;
+		public int value = 5;
+		public string image = ""; // image file location
+
+		public virtual void OnLeftClick()
+		{
+			Recycle();
+		}
+
+		public virtual void OnRightClick()
+		{
+			Trash();
+		}
+
+		private void Recycle()
+		{
+
+		}
+
+		private void Trash()
+		{
+
+		}
+	}
+}
