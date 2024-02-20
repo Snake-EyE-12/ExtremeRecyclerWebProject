e62bcc4f1dce77ec0a2f90d1287ec9ba40eccd99
JaydenSmith43 <123499904+JaydenSmith43@users.noreply.github.com>
2024-02-19 23:36:01 -0700

    right click button test
    uses jquery

---
+++ Controllers/ButtonTestController.cs
@@ -0,0 +1,48 @@
+﻿using ExtremeRecycler.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExtremeRecycler.Controllers
+{
+	public class ButtonTestController : Controller
+	{
+		static List<ButtonModel> buttons = new List<ButtonModel>();
+		Random random = new Random();
+
+		public ButtonTestController()
+		{
+			if (buttons.Count == 0)
+			{
+				for (int i = 0; i < 25; i++)
+				{
+					if (random.Next(10) > 5) buttons.Add(new ButtonModel(true, false));
+					else buttons.Add(new ButtonModel(false, false));
+				}
+				buttons[0].Flagged = true;
+			}
+		}
+
+		public IActionResult Index()
+		{
+			return View("ButtonTest", buttons);
+		}
+
+		public ActionResult OnButtonClick(string toggle)
+		{
+			int buttonNumber = Int32.Parse(toggle);
+
+			if (!buttons[buttonNumber].Flagged)
+			{
+				buttons[buttonNumber].State = !buttons[buttonNumber].State;
+			}
+
+			return View("ButtonTest", buttons);
+		}
+
+		public ActionResult OnRightButtonClick(string toggle)
+		{
+			int buttonNumber = Int32.Parse(toggle);
+			buttons[buttonNumber].Flagged = !buttons[buttonNumber].Flagged;
+			return View("ButtonTest", buttons);
+		}
+	}
+}
---
+++ Models/ButtonModel.cs
@@ -0,0 +1,14 @@
+﻿namespace ExtremeRecycler.Models
+{
+	public class ButtonModel
+	{
+		public bool State { get; set; }
+		public bool Flagged { get; set; }
+
+		public ButtonModel(bool state, bool flagged)
+		{
+			State = state;
+			Flagged = flagged;
+		}
+	}
+}
