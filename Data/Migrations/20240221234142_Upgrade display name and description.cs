e23623f2181d3aed52852a9b306cc15c5fec2162
Jacob Leonardo <leonardojacob257@gmail.com>
2024-02-21 17:00:12 -0700

    Item and upgrade additions

---
+++ Data/Migrations/20240221234142_Upgrade display name and description.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExtremeRecycler.Data.Migrations
+{
+    public partial class Upgradedisplaynameanddescription : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Upgrades",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DisplayName",
+                table: "Upgrades",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Upgrades");
+
+            migrationBuilder.DropColumn(
+                name: "DisplayName",
+                table: "Upgrades");
+        }
+    }
+}
